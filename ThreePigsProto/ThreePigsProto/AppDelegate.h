f86ff314ea3d4e25b6ccc2786fd098faaa899c46
mssngpeces <mssngpeces@Editroom-2.local>
2018-07-10 10:43:47 -0400

    initial commit

---
+++ ThreePigsProto/ThreePigsProto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThreePigsProto
+//
+//  Created by mssngpeces on 7/10/18.
+//  Copyright © 2018 gabejacobs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ThreePigsProto/ThreePigsProto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ThreePigsProto
+//
+//  Created by mssngpeces on 7/10/18.
+//  Copyright © 2018 gabejacobs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
